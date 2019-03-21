ce8555355521873877f90e451899fe55397fce1b
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-21 11:37:40 -0400

    Added frames for methods

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,41 @@
-public class MyDeque{
+public class MyDeque<E> {
+
+  public static void main(String[] args) {
+
+  }
+
+  private E[] data ;
+  private int size, start, end ;
+
+  public MyDeque() {
+    
+  }
+  public MyDeque(int initialCapacity) {
+
+  }
+  public int size() {
+
+  }
+  public String toString() {
+
+  }
+  public void addFirst(E element) {
+
+  }
+  public void addLast(E element) {
+
+  }
+  public E removeFirst() {
+
+  }
+  public E removeLast() {
+
+  }
+  public E getFirst() {
+
+  }
+  public E getLast() {
+
+  }
 
 }
