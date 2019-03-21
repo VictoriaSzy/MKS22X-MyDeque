83ffa1ca93f8bca9c51595e47a710370e6167d4d
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-21 11:43:01 -0400

    Worked on constructor

--- MyDeque.java
+++ MyDeque.java
@@ -1,23 +1,26 @@
 public class MyDeque<E> {
-
-  public static void main(String[] args) {
-
-  }
-
   private E[] data ;
   private int size, start, end ;
 
+  public static void main(String[] args) {
+    System.out.println() ;
+  }
+
+  @SuppressWarnings("unchecked")
   public MyDeque() {
-    
+    data = (E[])new Object[10] ;
+    size = 10 ;
   }
   public MyDeque(int initialCapacity) {
-
+    size = initialCapacity ;
+    data = (E[])new Object[size] ;
   }
   public int size() {
-
+    return size ;
   }
   public String toString() {
-
+    String res = "" ;
+    return res ;
   }
   public void addFirst(E element) {
 
