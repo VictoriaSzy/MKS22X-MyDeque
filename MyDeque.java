ef74f7066de7d9995ab72aa9f01edcb2dbdb9ffe
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-21 11:45:52 -0400

    Finished accessor methods

--- MyDeque.java
+++ MyDeque.java
@@ -10,14 +10,20 @@ public class MyDeque<E> {
   public MyDeque() {
     data = (E[])new Object[10] ;
     size = 10 ;
+    start = 0 ;
+    end = 9 ;
   }
   public MyDeque(int initialCapacity) {
     size = initialCapacity ;
     data = (E[])new Object[size] ;
+    start = 0 ;
+    end = size - 1 ;
   }
   public int size() {
     return size ;
   }
+  /* {a b c d }  / {}  /   {VALUE_VALUE2_VALUE3_}  (space after each value)
+  */
   public String toString() {
     String res = "" ;
     return res ;
@@ -34,11 +40,12 @@ public class MyDeque<E> {
   public E removeLast() {
 
   }
+  // accessor methods
   public E getFirst() {
-
+    return data[start] ;
   }
   public E getLast() {
-
+    return data[end] ;
   }
 
 }
