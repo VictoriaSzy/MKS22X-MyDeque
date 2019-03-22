3081a18b9395d662bb15083b5e2b972fa0464b2f
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-21 22:32:15 -0400

    Tried working with add and remove methods

--- MyDeque.java
+++ MyDeque.java
@@ -13,6 +13,7 @@ public class MyDeque<E> {
     start = 0 ;
     end = 9 ;
   }
+  @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity) {
     size = initialCapacity ;
     data = (E[])new Object[size] ;
@@ -20,25 +21,45 @@ public class MyDeque<E> {
     end = size - 1 ;
   }
   public int size() {
+    // returning capacity
     return size ;
   }
   /* {a b c d }  / {}  /   {VALUE_VALUE2_VALUE3_}  (space after each value)
   */
   public String toString() {
-    String res = "" ;
-    return res ;
+    String res = "{ " ;
+    for (int i = 0 ; i < end + 1 ; i++) {
+
+    }
+
+    return res + "}" ;
   }
   public void addFirst(E element) {
-
+    start = start - 1 ;
   }
+  @SuppressWarnings("unchecked")
   public void addLast(E element) {
-
+    if (end == data.length - 1) {
+      // we have our end at the end of data --> we have to make a new data
+      E[] d = (E[])new Object[data.length * 2] ;
+      for (int i = 0 ; i < data.length ; i++) {
+        if (data[i] != null) {
+          d[i] = data[i] ;
+        }
+      }
+    }
+    else {
+      end++ ;
+      data[end] = element ;
+    }
   }
   public E removeFirst() {
-
+    start++ ;
+    return data[start + 1] ;
   }
   public E removeLast() {
-
+    end-- ;
+    return data[end + 1] ;
   }
   // accessor methods
   public E getFirst() {
