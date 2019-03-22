f7b31dc30561b7e10c8475e328468b7bb223aed6
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-22 11:46:13 -0400

    Went back to size method

--- MyDeque.java
+++ MyDeque.java
@@ -7,25 +7,31 @@ public class MyDeque<E> {
   private int size, start, end ;
 
   public static void main(String[] args) {
-    System.out.println() ;
+    System.out.println("************************************************************************************") ;
     MyDeque a = new MyDeque() ;
-    System.out.println(a.toString()) ;
+    System.out.println("Here is the toString method run on the new MyDeque a: ") ;
+    System.out.println(a.toString()) ; // expecting only one null to be printed since it's from start to end
+    System.out.println("Here is the entire array (including before/after start and end):\n" + a.toStringFull()) ;
+    System.out.println("The size of a should be 1: " + a.size()) ;
+    System.out.println("Start (should be 0): " + a.getStartIndex()) ;
+    System.out.println("End (should be 0): " + a.getEndIndex()) ;
   }
   public MyDeque() {
     data = (E[])new Object[10] ;
-    size = 10 ;
+    size = 0 ;
     start = 0 ;
-    end = 9 ;
+    end = 0 ;
   }
   public MyDeque(int initialCapacity) {
-    size = initialCapacity ;
-    data = (E[])new Object[size] ;
+    data = (E[])new Object[initialCapacity] ;
     start = 0 ;
-    end = size - 1 ;
+    end = 0 ;
   }
   public int size() {
     // returning capacity
-    return end - start + 1 ;
+    if (size == 0) return 0 ;
+    // otherwise we can find the difference
+    return end - start ;
   }
   /* {a b c d }  / {}  /   {VALUE_VALUE2_VALUE3_}  (space after each value)
   */
@@ -36,9 +42,24 @@ public class MyDeque<E> {
     }
     return res + "}" ;
   }
+  // additional toString method to print out entire array - may include elements before and/or after start and end
+  public String toStringFull() {
+    String res = "{ " ;
+    for (int i = 0 ; i < data.length ; i++) {
+      res += data[i] + " " ;
+    }
+    return res + " }" ;
+  }
   public void addFirst(E element) {
     if (element == null) {
       throw new NullPointerException("You can't add null to the beginning!") ;
+    }
+    if (size() == 0) {
+      // data does not have any elements so that's good!
+      data[0] = element ;
+      size++ ;
+      end++ ;
+
     }
     start = start - 1 ;
   }
@@ -88,6 +109,13 @@ public class MyDeque<E> {
       throw new NoSuchElementException("You cannot get the last element because it doesn't exist!") ;
     }
     return data[end] ;
+  }
+  // for beginning of testing
+  public int getStartIndex() {
+    return start ;
+  }
+  public int getEndIndex() {
+    return end ;
   }
 
 }
