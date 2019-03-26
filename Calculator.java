a3ea3df58a8a00cae9bc554935c00a78471821e3
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-25 21:02:51 -0400

    Instantiated MyDeque and began reading through to add values

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,13 @@ public class Calculator {
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s) {
-    
+      MyDeque<Double> a = new MyDeque<Double>() ;
+      String[] list = s.split(" ") ;
+      int i = 0 ;
+      String operations = "+-/*" ;
+      while (i < list.length && !operations.contains(list[i])) {
+        int val = Integer.parseInt(list[i]) ;
+        a.addFirst(1.0 * val) ;
+      }
     }
 }
