bdee5884b61ae1cd33e6ffbfeee361044012ac88
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-28 17:06:22 -0400

    Simplified calculator to only use one for loop

--- Calculator.java
+++ Calculator.java
@@ -5,25 +5,21 @@ public class Calculator {
     public static double eval(String s) {
       MyDeque<Double> a = new MyDeque<Double>() ;
       String[] list = s.split(" ") ;
-      String[] ops = new String[list.length - ((list.length / 2) + 1) + 1] ;
-      int i = 0 ;
       String operations = "+-/*" ;
-      while (i < list.length && !operations.contains(list[i])) {
-        // we are finding the values
-        int val = Integer.parseInt(list[i]) ;
-        System.out.println("The value being added is: " + val) ;
-        a.addLast(1.0 * val) ;
+      for (int i = 0 ; i < list.length ; i++) {
+        if (operations.contains(list[i])) {
+          double aa = a.removeLast() ;
+          double bb = a.removeLast() ;
+          a.addLast(operate(bb, aa, list[i])) ;
+        }
+        else {
+          int x = Integer.parseInt(list[i]) ;
+          a.addLast((double) x) ;
+        }
       }
-      int x = 0 ;
-      while (i < list.length && operations.contains(list[i])) {
-        // we are finding all of the operations
-        ops[x] = list[i] ;
-      }
-      for (String op : ops) {
-
-      }
+      return a.getLast() ;
     }
-    public static Double operate(Double a, Double b, String op) {
+    public static double operate(double a, double b, String op) {
       if (op.equals("+")) return a + b ;
       if (op.equals("-")) return a - b ;
       if (op.equals("*")) return a * b ;
